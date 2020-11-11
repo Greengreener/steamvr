b8c3d46267a1c1b8cd2eae3d4030dd90b6609510
tylerwhelan <Tyler.Whelan@studytafensw.edu.au>
2020-11-11 16:22:20 +1100

    You can give burgers to people.

---
+++ Assets/Ty Folder/Scripts/BurgerCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ty
+{
+    public class BurgerCounter : MonoBehaviour
+    {
+        [SerializeField] int burgerCount = 1;
+        [SerializeField] GameObject messageObject;
+        int currentCount = 0;
+
+        public void AddToCount()
+        {
+            currentCount++;
+            if (currentCount >= burgerCount)
+            {
+                CompleteCount();
+            }
+        }
+
+        private void CompleteCount()
+        {
+            messageObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ Assets/Ty Folder/Scripts/RegularHumanScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ty
+{
+    public class RegularHumanScript : MonoBehaviour
+    {
+        bool satisfied = false;
+
+        private void BecomeSatisfied()
+        {
+            satisfied = true;
+            FindObjectOfType<BurgerCounter>().AddToCount();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("brger") && !satisfied)
+            {
+                BecomeSatisfied();
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
