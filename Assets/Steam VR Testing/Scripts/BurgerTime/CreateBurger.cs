b3e93523d228fbab8d098f602eb3a08f810bee77
Greengreener <36747370+Greengreener@users.noreply.github.com>
2020-12-02 11:53:01 +1100

    Update

---
+++ Assets/Steam VR Testing/Scripts/BurgerTime/CreateBurger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class CreateBurger : MonoBehaviour
+{
+    [SerializeField] VrController controller;
+
+    void Start()
+    {
+        controller = GetComponent<VrController>();
+    }
+}
